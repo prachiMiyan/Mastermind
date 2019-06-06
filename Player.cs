b57b335159f853bdd39c4ac422133a2bff89fe0f
Prachi Miyan <pmiyan@asu.edu>
2019-06-05 21:42:40 -0700

    first commit

---
+++ Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Mastermind
+{
+    class Player
+    {
+        public int chance;
+        public int input;
+
+        public Player()
+        {
+            chance = 0;
+            input = 0;
+        }
+
+        public Boolean TakeInput()
+        {
+            String inputStr = Console.ReadLine();
+            if(inputStr.Length != 4) // to check if the input has 4 characters
+            {
+                Console.WriteLine("Input must be 4 digits long. Try again!");
+                return false;
+            }
+
+            if (int.TryParse(inputStr, out input)) // to check if the input is all numbers 
+            {
+                int rangeInt = input;
+                for(int i = 0; i< 4; ++i) // to check that all digits in the input lie between 1 and 6
+                {
+                    int num = rangeInt / ((Int32)Math.Pow(10, 4 - i - 1));
+                    rangeInt = rangeInt % ((Int32)Math.Pow(10, 4 - i - 1));
+                    if (num < 1 || num > 6)
+                    {
+                        Console.WriteLine("Digits must be within the range of 1 and 6. Try again!");
+                        return false;
+                    }
+                }
+                ++chance;
+            }
+            else // if the input is not an int convertible
+            {
+                Console.WriteLine("Input must be a number. Try again!");
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Mastermind
+{
+    class MasterMind
+    {
+        private static String guessNum;
+        private static String result = ""; // string containing + and -
+        private static Boolean win = false;
+        public static void Main(string[] args)
+        {
+            GenerateNum(); //guessNum generated
+
+            Player P = new Player();
+            while(P.chance < 10)
+            {
+                Console.WriteLine("Enter a number for chance " + (P.chance+1) + ": ");
+                if (!P.TakeInput())
+                    continue;
+                result = Compare(P.input);
+                if(result.Equals("++++"))
+                {
+                    win = true;
+                    break;
+                }
+                Console.WriteLine(result);
+            }
+
+            if (win == true)
+                Console.WriteLine("You have won!");
+            else
+                Console.WriteLine("You have lost. The number is " + guessNum + "!");
+        }
+
+        private static void GenerateNum() //generates a 4 digit random number as required and stores in guessNum
+        {
+            Random r = new Random();
+            int i = 0, num = 0;
+            while (i < 4)
+            {
+                num = r.Next(1, 7);
+                guessNum = String.Concat(guessNum, num);
+                ++i;
+            }
+            return;
+        }
+
+        private static String Compare(int input) //compares input with guessNum and returns result combination
+        {
+            String combo = "";
+            Char[] guess1 = guessNum.ToCharArray();
+            Char[] input1 = (input.ToString()).ToCharArray();
+            int x = 4;
+
+            for(int i = 0; i < x; ++ i) // to check corresponding numbers at same position
+            {
+                if (guess1[i] == input1[i])
+                {
+                    combo = String.Concat(combo, "+");
+                    guess1[i] = '$';
+                    input1[i] = '$';
+                }
+            }
+
+
+            for (int i = 0; i < x; ++i) // to check if the guessed input number exists anywhere in the guesses number besides the same position
+            {
+                String guess2 = new String(guess1);
+
+                if (input1[i] != '$' && guess2.IndexOf(input1[i])!= -1)
+                {
+                    combo = String.Concat(combo, "-");
+                    int ind = guess2.IndexOf(input1[i]);
+                    guess1[ind] = '$';
+                }
+            }
+            return combo;
+        }
+
+    }
+}
